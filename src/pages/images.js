b37b60f4b68c39dcaa76017f27aa926ce3d53299
CraftAtoms <creator@craftatoms.com>
2018-11-13 03:26:05 -0500

    Gitimg

--- src/pages/images.js
+++ src/pages/images.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import SSIMG from '../images/ssimg.png'
+import GITIMG from '../images/GITIMG.png'
 
 export default class images extends Component {
   render() {
     return (
       <div>
         <img src={SSIMG} alt='img'/>
+        <img src={GITIMG} alt='img'/>
       </div>
     )
   }
